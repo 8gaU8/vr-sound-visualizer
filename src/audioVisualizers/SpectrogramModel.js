e874f1922eddcf6b389cb19df241b0374bfe0a3b
8gaU8 <71952256+8gaU8@users.noreply.github.com>
2025-05-22 11:26:23 +0200

    add typing

--- src/audioVisualizers/SpectrogramModel.js
+++ src/audioVisualizers/SpectrogramModel.js
@@ -9,6 +9,36 @@ import vertShaer from './shaders/spectrogram.vert?raw'
 
 export class SpectrogramModel {
   /**
+   * @description The analyser for the audio
+   * @type {THREE.AudioAnalyser}
+   */
+  analyser
+
+  /**
+   * @description The uniforms for the shader
+   * @type {Object}
+   */
+  uniforms
+
+  /**
+   * @description The size of the FFT
+   * @type {Number}
+   */
+  fftSize
+
+  /**
+   * @description Current maximum amplitude
+   * @type {Number}
+   */
+  intensity = 0
+
+  /**
+   * @description The mesh for the spectrogram
+   * @type {THREE.Mesh}
+   */
+  mesh
+
+  /**
    * @param {THREE.Audio<AudioNode>} audio
    */
   constructor(audio) {
