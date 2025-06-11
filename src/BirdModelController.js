5ca969f79db39591f00c0de8068be998eb6c3f81
8gaU8 <71952256+8gaU8@users.noreply.github.com>
2025-06-11 10:51:50 +0200

    update model position in model controller

--- src/BirdModelController.js
+++ src/BirdModelController.js
@@ -1,13 +1,33 @@
 // @ts-check
-import { CatmullRomCurve3, Euler, Quaternion, Vector3 } from 'three'
+import { CatmullRomCurve3, Quaternion, Vector3 } from 'three'
 
 import { loaders } from './loaders'
 import { RNG } from './noise.js'
 
 /**
  * @typedef {import('three').Mesh} Mesh
+ * @typedef {import('three').Object3D} Object3D
  * @typedef {typeof import('./defaultConfigs/birds/woodpecker.json.js').config} BirdConfig
  */
+
+/**
+ *
+ * @param {BirdConfig} config
+ * @returns {Vector3[]} - Returns an array of Vector3 points representing the motion path of the bird.
+ */
+function generateMotion(config) {
+  const rng = new RNG(config.seed) // Seed for consistent random generation
+  const randomPoints = []
+  for (let i = 0; i < 20; i++) {
+    const randomX = rng.random() * 5 //+ config.position.x
+    const randomY = rng.random() * 3 //+ config.position.y
+    const randomZ = rng.random() * 5 //+ config.position.z
+    randomPoints.push(new Vector3(randomX, randomY, randomZ))
+  }
+  const curve = new CatmullRomCurve3(randomPoints, true, 'catmullrom', 0.5)
+  const points = curve.getPoints(2000)
+  return points
+}
 
 export class BirdModelController {
   /** @type {Mesh} - The 3D mesh of the bird model */
@@ -22,7 +42,7 @@ export class BirdModelController {
   constructor(config) {
     this.config = config
 
-    this.curvePoints = this.#generateMotion()
+    this.curvePoints = generateMotion(this.config)
   }
 
   /**
@@ -64,9 +84,6 @@ export class BirdModelController {
    * @returns {Vector3} - Returns the position of the bird model at the specified time.
    */
   getPosition(time) {
-    if (!this.config.motion) {
-      return new Vector3(this.config.position.x, this.config.position.y, this.config.position.z)
-    }
     const t = time * 0.05 // Convert time to seconds
     const index = Math.floor(t * this.curvePoints.length) % this.curvePoints.length
     const point = this.curvePoints[index]
@@ -79,9 +96,6 @@ export class BirdModelController {
    * @returns {Quaternion} - Returns the quaternion representing the rotation of the bird model at the specified time.
    */
   getQuaternion(time) {
-    if (!this.config.motion) {
-      return new Quaternion().setFromEuler(new Euler(0, 0, 0)) // No motion, no rotation
-    }
     const t = time * 0.05 // Convert time to seconds
     const index = Math.floor(t * this.curvePoints.length) % this.curvePoints.length
     const point = this.curvePoints[index]
@@ -95,18 +109,21 @@ export class BirdModelController {
     return quaternion.clone()
   }
 
-  #generateMotion() {
-    const rng = new RNG(this.config.seed) // Seed for consistent random generation
+  /**
+   * @description Updates the position and rotation of the bird model based on the elapsed time.
+   * @param {number} time
+   * @param {Object3D} model
+   */
+  updateModelPosition(time, model) {
+    // If motion is disabled, do not update position or rotation
+    if (!this.config.motion) return
 
-    const randomPoints = []
-    for (let i = 0; i < 20; i++) {
-      const randomX = rng.random() * 5
-      const randomY = rng.random() * 3
-      const randomZ = rng.random() * 5
-      randomPoints.push(new Vector3(randomX, randomY, randomZ))
-    }
-    const curve = new CatmullRomCurve3(randomPoints, true, 'catmullrom', 0.5)
-    const points = curve.getPoints(2000)
-    return points
+    // update model position
+    const position = this.getPosition(time)
+    model.position.copy(position)
+
+    // update model rotation
+    const quaternion = this.getQuaternion(time)
+    model.quaternion.copy(quaternion)
   }
 }
