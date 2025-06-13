f8cfe350d384a66ef7dd369cb7b030d55296633d
8gaU8 <71952256+8gaU8@users.noreply.github.com>
2025-06-13 11:31:24 +0200

    adjust size

--- public/configs/woodpecker.json.js
+++ public/configs/woodpecker.json.js
@@ -6,7 +6,7 @@ export const config = {
   modelPath: 'woodpecker.glb',
   audioPath: 'PileatedWoodpecker.mp3',
   position: { x: 2, y: 1, z: -4 },
-  scale: { x: 0.5, y: 0.5, z: 0.5 },
+  scale: { x: 0.25, y: 0.25, z: 0.25 },
   color: 0xff206e,
   motion: true,
   spectrogramPosition: {
